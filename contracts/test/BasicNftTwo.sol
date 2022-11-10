10890faf02344cf78f228e625ca4319f2c98bdbc
xx <xx>
2022-11-10 21:53:20 +0800

    update

--- contracts/test/BasicNftTwo.sol
+++ contracts/test/BasicNftTwo.sol
@@ -2,9 +2,16 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract BasicNftTwo is ERC721 {
+    using Strings for uint256;
+
     string public constant TOKEN_URI = "ipfs://QmdryoExpgEQQQgJPoruwGJyZmz6SqV4FRTX1i73CT3iXn";
+
+    string internal baseURI = "ipfs://QmciivWCFZP7tRQBYg3FkTHaUpg8FLsN2wcWJA8ik7cf2H/";
+    string internal uriSuffix = ".json";
+
     uint256 private s_tokenCounter;
 
     event DogMinted(uint256 indexed tokenId);
@@ -19,9 +26,13 @@ contract BasicNftTwo is ERC721 {
         s_tokenCounter = s_tokenCounter + 1;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-        return TOKEN_URI;
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        require(_exists(_tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        return
+            bytes(baseURI).length > 0
+                ? string(abi.encodePacked(baseURI, _tokenId.toString(), uriSuffix))
+                : "";
     }
 
     function getTokenCounter() public view returns (uint256) {
